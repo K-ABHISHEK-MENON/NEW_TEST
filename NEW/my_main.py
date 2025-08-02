371b02fc6be375e2b0de5a1ee82ef99b7e45eb08
Abhishek <kabhishekmenon@gmail.com>
2025-08-02 12:05:26 +0530

    FIRST CHANGE

--- NEW/my_main.py
+++ NEW/my_main.py
@@ -34,14 +34,7 @@ def main():
     avg_rate = np.mean(rate_user_matrix[:, :iter_index], axis=0)
     plt.plot(range(1, iter_index + 1), avg_rate, 'k--', linewidth=2.5, label='Average')
 
-    plt.xlabel('Iteration Index')
-    plt.ylabel('Data Rate (bps/Hz)')
-    plt.title('Per-User and Average Data Rates over Iterations')
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig('per_user_data_rate.png')
-    plt.show()
+    
 
 if __name__ == "__main__":
     main()
